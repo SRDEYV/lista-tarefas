ec306f6ffbc7567d6428d8efcae5c3808531a7e8
Deyvison Santos <deyvison.s.santos15@gmail.com>
2024-06-19 10:18:11 -0400

    Finalizado

--- scripts.js
+++ scripts.js
@@ -9,12 +9,11 @@ function AddTask() {
 
         tarefa: input.value,
         StatusTask: false
-    }
-    )
-    
+    })
+
     input.value = ''
 
-   
+
     mostrarTarefa()
 }
 
@@ -24,8 +23,8 @@ function mostrarTarefa() {
     ListaItens.forEach((item, posicao) => {
 
         novaLi = novaLi + `
-            <li class="task">
-                <img src="./img/checked.png" alt="check-tarefa">
+            <li class="task ${item.StatusTask && "done"}">
+                <img src="./img/checked.png" alt="check-tarefa" onclick="concluirtarefa(${posicao})">
                 <input class="input-QTD" placeholder="quant" name="" id="quantidade">
                 <p>${item.tarefa}</p>
                 <img src="./img/trash.png" alt="tarefa-para-lixo" onclick="deletariten(${posicao})">
@@ -35,9 +34,21 @@ function mostrarTarefa() {
 
     ListaCompleta.innerHTML = novaLi
 
+    localStorage.setItem('lista', JSON.stringify(ListaItens))
+
 
 
 }
+
+function recarregarTarefas(){
+    const tarefaDoLocalStorage = localStorage.getItem('lista')
+
+    ListaItens = JSON.parse(tarefaDoLocalStorage)
+    console.log(tarefaDoLocalStorage)
+    mostrarTarefa()
+}
+
+recarregarTarefas()
 
 function deletariten(posicao) {
     ListaItens.splice(posicao, 1)
@@ -46,9 +57,10 @@ function deletariten(posicao) {
 
 }
 
-function concluirtarefa() {
-
-
+function concluirtarefa(posicao) { //é o colo onclicer no hltml, para saber qual intem foi clicado e informar a posição
+    ListaItens[posicao].StatusTask = !ListaItens[posicao].StatusTask
+    mostrarTarefa()
+    console.log(posicao)
 }
 
 button.addEventListener('click', AddTask)
